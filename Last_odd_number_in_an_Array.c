04a8e6096b63ec67df1d2172204837439a14d89a
shariff0204 <145580222+shariff0204@users.noreply.github.com>
2024-01-18 18:28:08 +0530

    #include<stdio.h> int main() {     int a,i;     scanf("%d",&a);     int arr[a];     for(i=0;i<a;i++)     {         scanf("%d",&arr[i]);     }     for(i=a-1;i<a;i--)     {         if(arr[i]%2!=0)         {             printf("%d",arr[i]);             break;         }     } }

---
+++ Last_odd_number_in_an_Array.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int a,i;
+    scanf("%d",&a);
+    int arr[a];
+    for(i=0;i<a;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=a-1;i<a;i--)
+    {
+        if(arr[i]%2!=0)
+        {
+            printf("%d",arr[i]);
+            break;
+        }
+    }
+}
